93828425c1f9477bf4fc10578cc04f163d795ccb
shohabbos94 <99381998+shohabbos94@users.noreply.github.com>
2022-08-13 14:01:10 +0500

    init

--- data/config.py
+++ data/config.py
@@ -1,10 +1,16 @@
-from environs import Env
+# from environs import Env
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+
+import os
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN")) #bot tokenni olish
+ADMINS = list(os.environ.get("ADMINS")) # Adminlar ro'yxatini yuklab olish uchun
+IP = str(os.environ.get("ip")) # Hosting ip manzili
+# PROVIDER_TOKEN = str(os.environ.get("PROVIDER_TOKEN")) 
